2005c0f0769b58fe46dbd0d4b3fbfd0e87a5d921
radekp00 <radekp00@gmail.com>
2024-05-08 12:52:52 +0000

    4_1 oraz 4_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,58 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    while (true) {
+
+      int pos = 0;
+      System.out.println("0. Wyjscie");
+      System.out.println("1. Dodawanie studenta");
+      System.out.println("2. Wyswieltanie studentow");
+
+      pos = myObj.nextInt();
+
+      switch (pos) {
+        case 0:
+          System.exit(00);
+        case 1:
+          dodajStudenta();
+          break;
+        case 2:
+          zobaczStudentow();
+          break;
+        default:
+
+          break;
+
+      }
+
+    }
+  }
+
+  public static void dodajStudenta() {
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Podaj imie studenta: ");
+    String imie = myObj.nextLine();
+    System.out.println("Podaj wiek studenta: ");
+    int wiek = myObj.nextInt();
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      s.addStudent(new Student(imie, wiek));
+    } catch (IOException e) {
+
+    }
+  }
+
+  public static void zobaczStudentow() {
+    try {
+      Service s = new Service();
 
       var students = s.getStudents();
-      for(Student current : students) {
+      for (Student current : students) {
         System.out.println(current.ToString());
       }
     } catch (IOException e) {
